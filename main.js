473618e52376241a85b39a3e739b9bd5340c4340
Jonathan Mani <jonathanmani@protonmail.com>
2022-06-15 18:28:30 -0400

    fixed issues with the examples sentences not rendering properly

--- main.js
+++ main.js
@@ -6,33 +6,31 @@ function getDefinition(){
         .then(data => {
             console.log(data[0])
             document.querySelector('.word').innerHTML = data[0].meta.stems[0].toUpperCase();
-            document.querySelector('.pronounciation').innerHTML = data[0].hwi.hw;
-            getDefinition(data[0]);
+            getShortDef(data[0]);
             getSentence(data[0]);
-            
-            
         })
+}
 
-        function getDefinition(data){
-            document.querySelector('.def01').innerHTML = data.shortdef[0];
-            if(data.shortdef[1] != null){
-                document.querySelector('.def02').innerHTML = data.shortdef[1];
-            }
-        }
-            
-        
-        function getSentence(data){
-            document.querySelector('.sentence01').innerHTML = '';
-            document.querySelector('.sentence02').innerHTML = '';
-            
-            if (data.def[0].sseq[0][0][1].dt[1][1][0] !=null){
-                document.querySelector('.sentence01').innerHTML = data.def[0].sseq[0][0][1].dt[1][1][0].t;
-            }
-            
-            if(data.def[0].sseq[0][0][1].dt[1][1][1] !=null){
-                document.querySelector('.sentence02').innerHTML = data.def[0].sseq[0][0][1].dt[1][1][1].t;
-            }
-        }
-
-
+function getShortDef(data){
+    document.querySelector('.def01').innerHTML = data.shortdef[0];
+    if(data.shortdef[1] != null){
+        document.querySelector('.def02').innerHTML = data.shortdef[1];
+    }
+}
+    
+function getSentence(data){
+    document.querySelector('.sentence01').innerHTML = '';
+    document.querySelector('.sentence02').innerHTML = '';
+    let sentence01 = data.def[0].sseq[0][0][1].dt[1][1][0].t;
+    let sentence02 = data.def[0].sseq[0][0][1].dt[1][1][1].t;
+    
+    if (sentence01 !=null){
+        let sentence01Replace = sentence01.replace('{wi}','').replace('{/wi}','');
+        document.querySelector('.sentence01').innerHTML =`// ${sentence01Replace}`;
+    }
+    
+    if(sentence02 !=null){
+        let sentence02Replace = sentence02.replace('{wi}','').replace('{/wi}','');
+        document.querySelector('.sentence02').innerHTML =`// ${sentence02Replace}`;
+    }
 }
