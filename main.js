06674e93a08cd3f336d1a01643500f01bb235c31
Jonathan Mani <jonathanmani@protonmail.com>
2022-06-15 13:30:07 -0400

    created functions to get definitions and sentences

--- main.js
+++ main.js
@@ -7,23 +7,31 @@ function getDefinition(){
             console.log(data[0])
             document.querySelector('.word').innerHTML = data[0].meta.stems[0].toUpperCase();
             document.querySelector('.pronounciation').innerHTML = data[0].hwi.hw;
-            document.querySelector('.def01').innerHTML = data[0].shortdef[0];
-            document.querySelector('.def02').innerHTML = data[0].shortdef[1];
+            getDefinition(data[0]);
             getSentence(data[0]);
             
             
         })
+
+        function getDefinition(data){
+            document.querySelector('.def01').innerHTML = data.shortdef[0];
+            if(data.shortdef[1] != null){
+                document.querySelector('.def02').innerHTML = data.shortdef[1];
+            }
+        }
+            
         
         function getSentence(data){
             document.querySelector('.sentence01').innerHTML = '';
             document.querySelector('.sentence02').innerHTML = '';
-            if (data.quotes == undefined){
-                document.querySelector('.sentence02').innerHTML = data.def[0].sseq[0][0][1].sdsense.dt[1][1][0].t;
-            } else {
-                document.querySelector('.sentence01').innerHTML = data.quotes[0]?.t;
-
+            
+            if (data.def[0].sseq[0][0][1].dt[1][1][0] !=null){
+                document.querySelector('.sentence01').innerHTML = data.def[0].sseq[0][0][1].dt[1][1][0].t;
             }
-
+            
+            if(data.def[0].sseq[0][0][1].dt[1][1][1] !=null){
+                document.querySelector('.sentence02').innerHTML = data.def[0].sseq[0][0][1].dt[1][1][1].t;
+            }
         }
 
 
